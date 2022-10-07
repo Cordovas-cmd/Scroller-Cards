08700f50530f0ddc005c600c783f0be1d644e1ff
samuel cordova <cordovas1337@gmail.com>
2022-10-06 21:35:43 -0500

    working on the script will figure more out tomorrow tired

--- script.js
+++ script.js
@@ -5,7 +5,19 @@ window.addEventListener('scroll', checkBoxes)
 
 // console log innerHeight to check where to animate. 
 
-checkBoxes();
+// checkBoxes();
 function checkBoxes() {
-    console.log(window.innerHeight)
+const triggerBox = window.innerHeight / 5 * 4
+
+boxes.forEach(box => {
+    // this method returns a DOMrect object providing information about the size of an element and it's postion relative to the viewport;
+    const boxTop= box.getBoundingClientRect().top
+
+    if(boxTop < triggerBox) {
+        box.classList.add('show')
+    } else {
+
+        box.classList.remove('show')
+    }
+})
 }
